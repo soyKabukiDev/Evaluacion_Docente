e43bf7323cbbcaa7e8918ff256875c25345a9231
soyKabukiDev <mrlatec2001@gmail.com>
2024-07-10 17:09:39 -0600

    inicio del desarrollo

---
+++ src/app/dasboardAdministracion/layout-admin/dashhome/dashhome.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashhome',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashhome.component.html',
+  styleUrl: './dashhome.component.scss'
+})
+export class DashhomeComponent {
+
+}
