a9cbce75be56ef0e3efdd580abca7558dfb7e039
soyKabukiDev <mrlatec2001@gmail.com>
2024-07-10 16:05:42 -0600

    Roll back a estado inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PruebaComponent } from './components/prueba/prueba.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PruebaComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Evaluacion Docente';
-  email: string = 'tome@xample.com';
+  title = 'DocentesUPVM';
 }
