73cc551d563b65c11ed974385fc71bfaad038c8d
soyKabukiDev <mrlatec2001@gmail.com>
2024-07-10 16:49:06 -0600

    Comentario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FlowbiteService } from './services/flowbite.service';
+
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,14 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'DocentesUPVM';
+  constructor(private flowbiteService: FlowbiteService) {}
+
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite(flowbite => {
+      console.log('Flowbite loaded ✅', flowbite);
+    });
+  }
+  
 }
