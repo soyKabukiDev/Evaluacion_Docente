ee41dccb8f8d2b2c7f7a667e08139fa9c29d4cb2
soyKabukiDev <mrlatec2001@gmail.com>
2024-07-19 00:29:41 -0600

    actualizaciond coments

--- src/app/admin/administraitor/homeadmin/homeadmin.component.ts
+++ src/app/admin/administraitor/homeadmin/homeadmin.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {FormSettingsComponent} from "./form-settings/form-settings.component";
+import { RatingtableComponent } from '../teachersdata/ratingtable/ratingtable.component';
 
 @Component({
   selector: 'app-homeadmin',
   standalone: true,
-  imports: [FormSettingsComponent],
+  imports: [FormSettingsComponent, RatingtableComponent],
   templateUrl: './homeadmin.component.html',
   styleUrl: './homeadmin.component.scss'
 })
