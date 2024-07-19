c8917e6a0a3bcc09b4976d4eba26e9abdf556bcc
soyKabukiDev <mrlatec2001@gmail.com>
2024-07-18 22:40:17 -0600

    actualizacion card y periodos

--- src/app/admin/administraitor/homeadmin/form-settings/form-settings.component.ts
+++ src/app/admin/administraitor/homeadmin/form-settings/form-settings.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-settings',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form-settings.component.html',
   styleUrl: './form-settings.component.scss'
 })
-export default class FormSettingsComponent {
+export class FormSettingsComponent {
+  toggle1 = false;
+  toggle2 = false;
+  toggle3 = false;
+  period: string = 'Enero - Abril';
 
+  toggleChanged(toggleNumber: number, event: Event) {
+    const isChecked = (event.target as HTMLInputElement).checked;
+
+    if (isChecked) {
+      this.toggle1 = toggleNumber === 1;
+      this.toggle2 = toggleNumber === 2;
+      this.toggle3 = toggleNumber === 3;
+
+      if (toggleNumber === 1) {
+        this.period = 'Enero - Abril';
+      } else if (toggleNumber === 2) {
+        this.period = 'Mayo - Agosto';
+      } else if (toggleNumber === 3) {
+        this.period = 'Septiembre - Diciembre';
+      }
+
+    console.log(`Toggle ${this.period} changed:`, isChecked );
+  }
+  }
 }
--- src/app/admin/administraitor/homeadmin/homeadmin.component.ts
+++ src/app/admin/administraitor/homeadmin/homeadmin.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {FormSettingsComponent} from "./form-settings/form-settings.component";
 
 @Component({
   selector: 'app-homeadmin',
   standalone: true,
-  imports: [],
+  imports: [FormSettingsComponent],
   templateUrl: './homeadmin.component.html',
   styleUrl: './homeadmin.component.scss'
 })
